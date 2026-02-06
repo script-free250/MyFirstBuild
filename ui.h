efed5176e73d742ea6f3dec9a0a93d931e4d832a
free-script <mostafanitro80@gmail.com>
2026-02-06 15:42:29 +0200

    Update ui.h

--- ui.h
+++ ui.h
@@ -5,6 +5,9 @@
 #include <windows.h>
 #include "imgui/imgui.h"
 
+// التوقيع السري لتمييز ضغطات برنامجنا (يمنع التعليق نهائياً)
+#define MAGIC_SIGNATURE 0xFF99
+
 enum class ActionType { SinglePress, RapidFire };
 
 struct KeyAction {
@@ -14,25 +17,21 @@ struct KeyAction {
     DWORD lastExecutionTime = 0; 
 };
 
-// --- Global Data ---
+// Global Data
 extern std::map<int, std::vector<KeyAction>> g_complex_mappings;
-extern std::map<int, bool> g_physical_key_status; 
+extern std::map<int, bool> g_physical_key_status; // حالة الزر الحقيقية (إصبع المستخدم)
 extern std::map<int, float> g_key_states;
 extern int g_last_vk_code;
 extern std::string g_last_key_name;
-extern bool g_safety_mode_active; 
-
-// متغيرات جديدة للإدارة
-extern std::vector<std::pair<int, bool>> g_pending_inputs; // طابور انتظار للإرسال الآمن
 
 enum class AppState {
-    Dashboard, Wizard_WaitForOriginal, Wizard_WaitForTarget, Wizard_AskSwap, Wizard_Configure        
+    Dashboard, Wizard_WaitForOriginal, Wizard_WaitForTarget, Wizard_Configure        
 };
 extern AppState g_app_state;
 
 void InstallHooks();
 void UninstallHooks();
-void ProcessInputLogic(); // دالة المعالجة المجمعة
+void ProcessInputLogic(); 
 void RenderUI();
 
 std::string GetKeyNameSmart(int vkCode);
