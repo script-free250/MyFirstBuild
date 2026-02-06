a0b58e7880600fe34675270b8e06049e9ab9cfc7
free-script <mostafanitro80@gmail.com>
2026-02-06 15:23:45 +0200

    Update ui.h

--- ui.h
+++ ui.h
@@ -5,11 +5,7 @@
 #include <windows.h>
 #include "imgui/imgui.h"
 
-// --- Data Structures ---
-enum class ActionType {
-    SinglePress, 
-    RapidFire    
-};
+enum class ActionType { SinglePress, RapidFire };
 
 struct KeyAction {
     int targetVk;       
@@ -18,29 +14,22 @@ struct KeyAction {
     DWORD lastExecutionTime = 0; 
 };
 
-// --- Global Data ---
 extern std::map<int, std::vector<KeyAction>> g_complex_mappings;
 extern std::map<int, bool> g_physical_key_status; 
 extern std::map<int, float> g_key_states;
 extern int g_last_vk_code;
 extern std::string g_last_key_name;
+extern bool g_safety_mode_active; 
 
-// --- App States (Wizard Flow) ---
 enum class AppState {
-    Dashboard,
-    Wizard_WaitForOriginal, 
-    Wizard_WaitForTarget,
-    Wizard_AskSwap,         // <--- خطوة جديدة: السؤال عن التبديل
-    Wizard_Configure        
+    Dashboard, Wizard_WaitForOriginal, Wizard_WaitForTarget, Wizard_AskSwap, Wizard_Configure        
 };
 extern AppState g_app_state;
 
-// --- Functions ---
 void InstallHooks();
 void UninstallHooks();
 void ProcessRapidFire();
 void UpdateAnimationState();
 void RenderUI();
 
-// Helpers
 std::string GetKeyNameSmart(int vkCode);
