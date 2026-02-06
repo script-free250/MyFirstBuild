d29eec5a76815e0ceecde9056adb162381e3a703
free-script <mostafanitro80@gmail.com>
2026-02-06 18:58:03 +0200

    Update keyboard_hook.h

--- keyboard_hook.h
+++ keyboard_hook.h
@@ -4,7 +4,7 @@
 #include <string>
 #include <vector>
 
-// Structures
+// الهياكل
 struct KeyStats {
     int pressCount = 0;
 };
@@ -15,7 +15,7 @@ struct Macro {
     std::vector<int> sequence;
 };
 
-// Global Variables
+// المتغيرات العامة
 extern std::map<int, int> g_key_mappings;
 extern std::map<int, KeyStats> g_key_stats;
 extern std::vector<Macro> g_macros;
@@ -24,8 +24,9 @@ extern bool g_turbo_mode_active;
 extern bool g_sound_enabled;
 extern int g_last_pressed_key;
 
-// Functions
-void InstallHook();
-void UninstallHook();
+// الدوال (تم تعديل الأسماء لتطابق main.cpp)
+void InstallHooks();       // كان اسمها InstallHook
+void UninstallHooks();     // كان اسمها UninstallHook
+void ProcessInputLogic();  // دالة جديدة مطلوبة
 void SaveSettings();
 void LoadSettings();
