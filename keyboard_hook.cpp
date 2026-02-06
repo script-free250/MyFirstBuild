254d68793052af2349a34c0f5d9b755d4a79459c
free-script <mostafanitro80@gmail.com>
2026-02-06 13:44:01 +0200

    Update keyboard_hook.cpp

--- keyboard_hook.cpp
+++ keyboard_hook.cpp
@@ -1,14 +1,65 @@
-#pragma once
-#include <map>
-#include <string>
+#include <windows.h>
+#include "keyboard_hook.h"
 
-// متغيرات مشتركة بين الواجهة والهوك
-extern std::map<int, int> g_key_mappings;
-extern std::map<int, int> g_key_states;
-extern int g_key_press_count;
-extern std::string g_last_key_name;
+// تعريف المتغيرات المشتركة
+std::map<int, int> g_key_mappings;
+std::map<int, int> g_key_states;
+int g_key_press_count = 0;
+std::string g_last_key_name = "None";
 
-void InstallHook();
-void UninstallHook();
-void AddOrUpdateMapping(int from, int to);
-void UpdateAnimationState();
+static HHOOK g_keyboardHook = NULL;
+
+LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
+    if (nCode == HC_ACTION) {
+        KBDLLHOOKSTRUCT* p = (KBDLLHOOKSTRUCT*)lParam;
+        if (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN) {
+            // تحديث الواجهة
+            g_key_press_count++;
+            char key_name[256];
+            GetKeyNameTextA(p->lParam, key_name, 256);
+            g_last_key_name = key_name;
+            g_key_states[p->vkCode] = 255; // بدء الأنيميشن
+
+            // تنفيذ التخصيص
+            if (g_key_mappings.count(p->vkCode)) {
+                int newVkCode = g_key_mappings[p->vkCode];
+                INPUT input = {0};
+                input.type = INPUT_KEYBOARD;
+                input.ki.wVk = newVkCode;
+                SendInput(1, &input, sizeof(INPUT));
+                return 1; 
+            }
+        }
+         if (wParam == WM_KEYUP || wParam == WM_SYSKEYUP) {
+            if (g_key_mappings.count(p->vkCode)) {
+                int newVkCode = g_key_mappings[p->vkCode];
+                INPUT input = {0};
+                input.type = INPUT_KEYBOARD;
+                input.ki.wVk = newVkCode;
+                input.ki.dwFlags = KEYEVENTF_KEYUP;
+                SendInput(1, &input, sizeof(INPUT));
+                return 1;
+            }
+        }
+    }
+    return CallNextHookEx(g_keyboardHook, nCode, wParam, lParam);
+}
+
+void InstallHook() {
+    g_keyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, LowLevelKeyboardProc, GetModuleHandle(NULL), 0);
+}
+
+void UninstallHook() {
+    if (g_keyboardHook) UnhookWindowsHookEx(g_keyboardHook);
+}
+
+void AddOrUpdateMapping(int from, int to) {
+    g_key_mappings[from] = to;
+}
+
+// دالة لتحديث حالة الأنيميشن (يتم استدعاؤها من الحلقة الرئيسية)
+void UpdateAnimationState() {
+    for (auto it = g_key_states.begin(); it != g_key_states.end(); ) {
+        it->second -= 5; // سرعة خفوت اللون
+        if (it->second <= 0) {
+
