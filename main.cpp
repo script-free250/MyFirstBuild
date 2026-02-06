a7bfa737e16000fbf59422ae5f6206cffd4343a2
free-script <mostafanitro80@gmail.com>
2026-02-06 12:54:19 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 
 #define IDC_GAME_BUTTON 101
 
-// نستخدم L"..." لتعريف النصوص كنصوص Unicode
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch (msg) {
         case WM_COMMAND:
@@ -16,35 +15,31 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
         case WM_DESTROY:
             PostQuitMessage(0);
             break;
-        // نستخدم DefWindowProcW وهي نسخة Unicode من الدالة
         default:
             return DefWindowProcW(hwnd, msg, wParam, lParam);
     }
     return 0;
 }
 
-// نستخدم LPWSTR بدلاً من LPSTR في تعريف الدالة
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow) {
-    WNDCLASSEXW wc = {0}; // نستخدم WNDCLASSEXW وهي نسخة Unicode
+// ----- التغيير الوحيد هنا: تم تغيير اسم الدالة إلى wWinMain -----
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow) {
+    WNDCLASSEXW wc = {0};
     wc.cbSize        = sizeof(WNDCLASSEXW);
     wc.lpfnWndProc   = WndProc;
     wc.hInstance     = hInstance;
     wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
     wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 2);
-    wc.lpszClassName = L"MyUnicodeWindowClass"; // اسم الفئة كنص Unicode
+    wc.lpszClassName = L"MyUnicodeWindowClass";
     wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
     wc.hIconSm       = LoadIcon(NULL, IDI_APPLICATION);
 
-    // نستخدم RegisterClassExW وهي نسخة Unicode
     if (!RegisterClassExW(&wc)) {
         MessageBoxW(NULL, L"Window Registration Failed!", L"Error!", MB_ICONEXCLAMATION | MB_OK);
         return 0;
     }
 
-    // نستخدم CreateWindowExW وهي نسخة Unicode
     HWND hwnd = CreateWindowExW(WS_EX_CLIENTEDGE, L"MyUnicodeWindowClass", L"لعبتي الأولى", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 480, 320, NULL, NULL, hInstance, NULL);
 
-    // نستخدم CreateWindowExW مرة أخرى للزر
     CreateWindowExW(0, L"BUTTON", L"اضغط هنا للعب!", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, 170, 120, 120, 40, hwnd, (HMENU)IDC_GAME_BUTTON, hInstance, NULL);
 
     ShowWindow(hwnd, nCmdShow);
