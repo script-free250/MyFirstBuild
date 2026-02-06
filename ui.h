1a0ac721f3d65ee7742f6f9fc4e0818de64188cc
free-script <mostafanitro80@gmail.com>
2026-02-06 19:33:47 +0200

    Update ui.h

--- ui.h
+++ ui.h
@@ -1,37 +1,8 @@
 #pragma once
-#include <map>
-#include <string>
-#include <vector>
-#include <windows.h>
-#include "imgui/imgui.h"
 
-// التوقيع السري لتمييز ضغطات برنامجنا (يمنع التعليق نهائياً)
-#define MAGIC_SIGNATURE 0xFF99
-
-enum class ActionType { SinglePress, RapidFire };
-
-struct KeyAction {
-    int targetVk;       
-    ActionType type;    
-    int delayMs;        
-    DWORD lastExecutionTime = 0; 
-};
-
-// Global Data
-extern std::map<int, std::vector<KeyAction>> g_complex_mappings;
-extern std::map<int, bool> g_physical_key_status; // حالة الزر الحقيقية (إصبع المستخدم)
-extern std::map<int, float> g_key_states;
-extern int g_last_vk_code;
-extern std::string g_last_key_name;
-
-enum class AppState {
-    Dashboard, Wizard_WaitForOriginal, Wizard_WaitForTarget, Wizard_Configure        
-};
-extern AppState g_app_state;
-
-void InstallHooks();
-void UninstallHooks();
-void ProcessInputLogic(); 
+// تعريف الدوال المشتركة للواجهة
 void RenderUI();
-
-std::string GetKeyNameSmart(int vkCode);
+void SetupStyle();
+void StartClickerThread();
+void StopClickerThread();
+void UpdateLogic();
