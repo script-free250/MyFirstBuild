6f058ad1590212e19fa67cda876d2938a7df6b64
free-script <mostafanitro80@gmail.com>
2026-02-06 18:58:55 +0200

    Update ui.cpp

--- ui.cpp
+++ ui.cpp
@@ -1,203 +1,66 @@
+#include "ui.h"
 #include "imgui/imgui.h"
-#include "imgui/imgui_impl_win32.h"
-#include "imgui/imgui_impl_dx11.h"
-#include <d3d11.h>
-#include <tchar.h>
-#include "ui.h" // يحتوي على دوال الهوك والمنطق
+#include "keyboard_hook.h"
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cstdio> // for sprintf
 
-// --- بيانات DirectX ---
-static ID3D11Device* g_pd3dDevice = NULL;
-static ID3D11DeviceContext* g_pd3dDeviceContext = NULL;
-static IDXGISwapChain* g_pSwapChain = NULL;
-static ID3D11RenderTargetView* g_mainRenderTargetView = NULL;
+static int selected_key_from = 0;
+static int selected_key_to = 0;
+static char search_buffer[128] = "";
+static int current_tab = 0;
 
-// تعريفات الدوال المساعدة
-bool CreateDeviceD3D(HWND hWnd);
-void CleanupDeviceD3D();
-void CreateRenderTarget();
-void CleanupRenderTarget();
+void RenderUI() {
+    ImGuiViewport* viewport = ImGui::GetMainViewport();
+    ImGui::SetNextWindowPos(viewport->Pos);
+    ImGui::SetNextWindowSize(viewport->Size);
+    ImGui::SetNextWindowFlags(ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoBringToFrontOnFocus);
 
-// معالج رسائل ويندوز الخارجي (من مكتبة ImGui)
-extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+    if (ImGui::Begin("MainUI", nullptr)) {
+        ImGui::TextColored(ImVec4(0.2f, 0.8f, 0.2f, 1.0f), "MY FIRST BUILD - ULTIMATE KEYBOARD UTILITY");
+        ImGui::Separator();
 
-// معالج الرسائل الرئيسي
-LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-    if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
-        return true;
+        if (ImGui::Button("Remap", ImVec2(100, 30))) current_tab = 0; ImGui::SameLine();
+        if (ImGui::Button("Stats", ImVec2(100, 30))) current_tab = 2; ImGui::SameLine();
+        if (ImGui::Button("Settings", ImVec2(100, 30))) current_tab = 3;
 
-    switch (msg)
-    {
-    case WM_SIZE:
-        if (g_pd3dDevice != NULL && wParam != SIZE_MINIMIZED)
-        {
-            CleanupRenderTarget();
-            g_pSwapChain->ResizeBuffers(0, (UINT)LOWORD(lParam), (UINT)HIWORD(lParam), DXGI_FORMAT_UNKNOWN, 0);
-            CreateRenderTarget();
-        }
-        return 0;
-    case WM_SYSCOMMAND:
-        if ((wParam & 0xfff0) == SC_KEYMENU) // تعطيل قائمة الزر الأيمن في العنوان لمنع التوقف
-            return 0;
-        break;
-    case WM_DESTROY:
-        ::PostQuitMessage(0);
-        return 0;
-    }
-    return ::DefWindowProc(hWnd, msg, wParam, lParam);
-}
+        ImGui::Separator();
 
-// نقطة الدخول الرئيسية
-int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE, LPTSTR, int nCmdShow)
-{
-    // 1. إنشاء نافذة التطبيق
-    WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, _T("ImGui App"), NULL };
-    ::RegisterClassEx(&wc);
-    HWND hwnd = ::CreateWindow(wc.lpszClassName, _T("Ultimate Input Remapper (Lite)"), WS_OVERLAPPEDWINDOW, 100, 100, 1000, 700, NULL, NULL, wc.hInstance, NULL);
-
-    // 2. تهيئة DirectX
-    if (!CreateDeviceD3D(hwnd))
-    {
-        CleanupDeviceD3D();
-        ::UnregisterClass(wc.lpszClassName, wc.hInstance);
-        return 1;
-    }
-
-    // إظهار النافذة
-    ::ShowWindow(hwnd, nCmdShow);
-    ::UpdateWindow(hwnd);
-
-    // 3. إعداد ImGui
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // تفعيل التنقل بالكيبورد
-
-    // ستايل داكن خفيف
-    ImGui::StyleColorsDark();
-
-    // ربط ImGui بالنظام
-    ImGui_ImplWin32_Init(hwnd);
-    ImGui_ImplDX11_Init(g_pd3dDevice, g_pd3dDeviceContext);
-
-    // 4. تشغيل الهوك (Hooks) - أهم خطوة للعمل
-    InstallHooks();
-
-    // 5. الحلقة الرئيسية (The Main Loop)
-    bool done = false;
-    while (!done)
-    {
-        // معالجة رسائل الويندوز (دون انتظار)
-        MSG msg;
-        while (::PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
-        {
-            ::TranslateMessage(&msg);
-            ::DispatchMessage(&msg);
-            if (msg.message == WM_QUIT)
-                done = true;
-        }
-        if (done)
-            break;
-
-        // ---[ CORE LOGIC ]---
-        // تشغيل منطق الأزرار والـ Rapid Fire
-        // يتم استدعاؤه دائماً حتى لو النافذة مصغرة لضمان عمل البرنامج في الخلفية
-        ProcessInputLogic();
-
-        // ---[ OPTIMIZATION ]---
-        // إذا كانت النافذة مصغرة (Minimized)، لا تقم بالرسم لتوفير الـ GPU/CPU
-        if (::IsIconic(hwnd))
-        {
-            // استراحة قصيرة جداً (1ms) للسماح للـ Rapid Fire بالعمل بدقة
-            // دون استهلاك المعالج بنسبة 100%
-            ::Sleep(1); 
-            continue; 
+        if (current_tab == 0) {
+            ImGui::Columns(2, "RemapCols", true);
+            ImGui::Text("Add New Mapping");
+            ImGui::InputInt("Key Code (From)", &selected_key_from);
+            ImGui::TextDisabled("Last Pressed: %d", g_last_pressed_key);
+            if (ImGui::Button("Use Last")) selected_key_from = g_last_pressed_key;
+            ImGui::InputInt("Key Code (To)", &selected_key_to);
+            if (ImGui::Button("Add Mapping")) {
+                if (selected_key_from != 0) g_key_mappings[selected_key_from] = selected_key_to;
+            }
+            ImGui::NextColumn();
+            ImGui::Text("Active Mappings");
+            for (auto it = g_key_mappings.begin(); it != g_key_mappings.end(); ) {
+                char label[64];
+                sprintf(label, "%d -> %d", it->first, it->second);
+                if (ImGui::Selectable(label)) {
+                    selected_key_from = it->first;
+                    selected_key_to = it->second;
+                }
+                ++it;
+            }
+            ImGui::Columns(1);
         }
 
-        // ---[ RENDERING ]---
-        // الرسم فقط عندما تكون النافذة ظاهرة
-        ImGui_ImplDX11_NewFrame();
-        ImGui_ImplWin32_NewFrame();
-        ImGui::NewFrame();
+        if (current_tab == 2) {
+            for (auto const& [key, stats] : g_key_stats) {
+                ImGui::Text("Key %d: %d presses", key, stats.pressCount);
+            }
+        }
 
-        // رسم واجهتنا الخاصة
-        RenderUI();
-
-        // إنهاء الإطار وعرضه
-        ImGui::Render();
-        const float clear_color_with_alpha[4] = { 0.1f, 0.1f, 0.12f, 1.0f }; // لون خلفية هادئ
-        g_pd3dDeviceContext->OMSetRenderTargets(1, &g_mainRenderTargetView, NULL);
-        g_pd3dDeviceContext->ClearRenderTargetView(g_mainRenderTargetView, clear_color_with_alpha);
-        ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
-        
-        // Present(1, 0) = VSync On (يمنع ارتفاع الـ FPS واستهلاك الكارت بلا داعي)
-        g_pSwapChain->Present(1, 0);
-
-        // استراحة 1ms لمنع استهلاك المعالج بالكامل في الحلقة
-        ::Sleep(1);
+        if (current_tab == 3) {
+            ImGui::Checkbox("Game Mode", &g_game_mode_active);
+            ImGui::Checkbox("Sound Effects", &g_sound_enabled);
+        }
     }
-
-    // 6. التنظيف عند الإغلاق
-    UninstallHooks(); // إزالة الهوك بأمان
-    
-    ImGui_ImplDX11_Shutdown();
-    ImGui_ImplWin32_Shutdown();
-    ImGui::DestroyContext();
-
-    CleanupDeviceD3D();
-    ::DestroyWindow(hwnd);
-    ::UnregisterClass(wc.lpszClassName, wc.hInstance);
-
-    return 0;
-}
-
-// --- دوال DirectX المساعدة (Boilerplate) ---
-
-bool CreateDeviceD3D(HWND hWnd)
-{
-    DXGI_SWAP_CHAIN_DESC sd;
-    ZeroMemory(&sd, sizeof(sd));
-    sd.BufferCount = 2;
-    sd.BufferDesc.Width = 0;
-    sd.BufferDesc.Height = 0;
-    sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-    sd.BufferDesc.RefreshRate.Numerator = 60;
-    sd.BufferDesc.RefreshRate.Denominator = 1;
-    sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
-    sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-    sd.OutputWindow = hWnd;
-    sd.SampleDesc.Count = 1;
-    sd.SampleDesc.Quality = 0;
-    sd.Windowed = TRUE;
-    sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
-
-    UINT createDeviceFlags = 0;
-    D3D_FEATURE_LEVEL featureLevel;
-    const D3D_FEATURE_LEVEL featureLevelArray[2] = { D3D_FEATURE_LEVEL_11_0, D3D_FEATURE_LEVEL_10_0, };
-    if (D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, createDeviceFlags, featureLevelArray, 2, D3D11_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice, &featureLevel, &g_pd3dDeviceContext) != S_OK)
-        return false;
-
-    CreateRenderTarget();
-    return true;
-}
-
-void CleanupDeviceD3D()
-{
-    CleanupRenderTarget();
-    if (g_pSwapChain) { g_pSwapChain->Release(); g_pSwapChain = NULL; }
-    if (g_pd3dDeviceContext) { g_pd3dDeviceContext->Release(); g_pd3dDeviceContext = NULL; }
-    if (g_pd3dDevice) { g_pd3dDevice->Release(); g_pd3dDevice = NULL; }
-}
-
-void CreateRenderTarget()
-{
-    ID3D11Texture2D* pBackBuffer;
-    g_pSwapChain->GetBuffer(0, IID_PPV_ARGS(&pBackBuffer));
-    g_pd3dDevice->CreateRenderTargetView(pBackBuffer, NULL, &g_mainRenderTargetView);
-    pBackBuffer->Release();
-}
-
-void CleanupRenderTarget()
-{
-    if (g_mainRenderTargetView) { g_mainRenderTargetView->Release(); g_mainRenderTargetView = NULL; }
+    ImGui::End();
 }
