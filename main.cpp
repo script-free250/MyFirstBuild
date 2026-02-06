74dd4f9249a6089a6394e0754babec4f082d5b1a
free-script <mostafanitro80@gmail.com>
2026-02-06 12:58:24 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,46 +1,78 @@
 #include <windows.h>
 
-#define IDC_GAME_BUTTON 101
+// سنحتاج إلى متغير عام للاحتفاظ بمؤشر (handle) على شاشة العرض النصية
+HWND hStaticDisplay;
 
+// دالة لمعالجة أحداث النافذة
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch (msg) {
-        case WM_COMMAND:
-            if (LOWORD(wParam) == IDC_GAME_BUTTON) {
-                MessageBoxW(hwnd, L"لقد فزت!", L"تهانينا", MB_OK | MB_ICONINFORMATION);
+        // يتم استدعاء هذه الحالة مرة واحدة فقط عند إنشاء النافذة
+        case WM_CREATE: {
+            // هنا سننشئ "شاشة" نصية ثابتة في منتصف النافذة
+            hStaticDisplay = CreateWindowExW(
+                0, L"STATIC",   // نوع العنصر: نص ثابت
+                L"Press any key...", // النص المبدئي
+                WS_CHILD | WS_VISIBLE | SS_CENTER, // الخصائص: يظهر في المنتصف
+                50, 100, 360, 40, // الإحداثيات والأبعاد
+                hwnd, NULL, NULL, NULL);
+
+            // تغيير حجم الخط ليبدو احترافياً
+            HFONT hFont = CreateFont(32, 0, 0, 0, FW_BOLD, FALSE, FALSE, FALSE, DEFAULT_CHARSET, OUT_OUTLINE_PRECIS,
+                CLIP_DEFAULT_PRECIS, CLEARTYPE_QUALITY, DEFAULT_PITCH, TEXT("Arial"));
+            SendMessage(hStaticDisplay, WM_SETFONT, (WPARAM)hFont, TRUE);
+            break;
+        }
+
+        // يتم استدعاء هذه الحالة عند الضغط على أي مفتاح في الكيبورد
+        case WM_KEYDOWN: {
+            // مصفوفة لتخزين اسم المفتاح
+            wchar_t keyName[256];
+            // lParam يحتوي على معلومات إضافية عن المفتاح
+            // GetKeyNameTextW هي دالة ويندوز سحرية تحول كود المفتاح إلى اسمه الحقيقي
+            if (GetKeyNameTextW(lParam, keyName, sizeof(keyName) / sizeof(wchar_t)) > 0) {
+                // نغير النص المعروض على الشاشة ليحمل اسم المفتاح الذي تم الضغط عليه
+                SetWindowTextW(hStaticDisplay, keyName);
             }
             break;
-        case WM_CLOSE:
-            DestroyWindow(hwnd);
+        }
+
+        // يتم استدعاء هذه الحالة عند رفع الإصبع عن المفتاح
+        case WM_KEYUP: {
+            // نعيد الشاشة إلى النص الافتراضي
+            SetWindowTextW(hStaticDisplay, L"Press any key...");
             break;
+        }
+
         case WM_DESTROY:
             PostQuitMessage(0);
             break;
+
         default:
             return DefWindowProcW(hwnd, msg, wParam, lParam);
     }
     return 0;
 }
 
-// ----- التغيير الوحيد هنا: تم تغيير اسم الدالة إلى wWinMain -----
+// نقطة بداية البرنامج
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow) {
     WNDCLASSEXW wc = {0};
     wc.cbSize        = sizeof(WNDCLASSEXW);
     wc.lpfnWndProc   = WndProc;
     wc.hInstance     = hInstance;
     wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
-    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 2);
-    wc.lpszClassName = L"MyUnicodeWindowClass";
-    wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
-    wc.hIconSm       = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW); // تغيير لون الخلفية للون النافذة الافتراضي
+    wc.lpszClassName = L"KeyboardTesterClass";
+    wc.hIcon         = LoadIcon(NULL, IDI_SHIELD); // تغيير أيقونة البرنامج
 
-    if (!RegisterClassExW(&wc)) {
-        MessageBoxW(NULL, L"Window Registration Failed!", L"Error!", MB_ICONEXCLAMATION | MB_OK);
-        return 0;
-    }
+    if (!RegisterClassExW(&wc)) return 0;
 
-    HWND hwnd = CreateWindowExW(WS_EX_CLIENTEDGE, L"MyUnicodeWindowClass", L"لعبتي الأولى", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 480, 320, NULL, NULL, hInstance, NULL);
-
-    CreateWindowExW(0, L"BUTTON", L"اضغط هنا للعب!", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, 170, 120, 120, 40, hwnd, (HMENU)IDC_GAME_BUTTON, hInstance, NULL);
+    HWND hwnd = CreateWindowExW(
+        WS_EX_CLIENTEDGE,
+        L"KeyboardTesterClass",
+        L"Professional Keyboard Tester", // عنوان احترافي
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 480, 320,
+        NULL, NULL, hInstance, NULL);
 
     ShowWindow(hwnd, nCmdShow);
     UpdateWindow(hwnd);
