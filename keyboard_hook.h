6555dc6c75c716a7fb5f97192ec21b4be1f6cf28
free-script <mostafanitro80@gmail.com>
2026-02-06 13:49:51 +0200

    Update keyboard_hook.h

--- keyboard_hook.h
+++ keyboard_hook.h
@@ -4,9 +4,9 @@
 
 // متغيرات مشتركة بين الواجهة والهوك
 extern std::map<int, int> g_key_mappings;
-extern std::map<int, int> g_key_states;
+extern std::map<int, int> g_key_states; // لتخزين حالة الأنيميشن
 extern int g_key_press_count;
-extern std::string g_last_key_name;
+extern int g_last_vk_code; // تم التغيير لتمرير الكود بدلاً من الاسم
 
 void InstallHook();
 void UninstallHook();
