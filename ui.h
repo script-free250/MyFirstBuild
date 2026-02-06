f323924b54b465ce06b5abf36256fc927864b869
free-script <mostafanitro80@gmail.com>
2026-02-06 14:17:47 +0200

    Update ui.h

--- ui.h
+++ ui.h
@@ -5,8 +5,9 @@
 #include "imgui/imgui.h"
 
 // Shared Global Variables
+// نستخدم float هنا لدعم الأنيميشن السلس
 extern std::map<int, int> g_key_mappings;
-extern std::map<int, float> g_key_states; // Float for smooth animation (0.0 to 1.0)
+extern std::map<int, float> g_key_states; 
 extern int g_key_press_count;
 extern int g_last_vk_code;
 extern std::string g_last_key_name;
