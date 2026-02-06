c855020dd3c49005a8c01513f8a28f9299026dce
free-script <mostafanitro80@gmail.com>
2026-02-06 15:33:31 +0200

    Update ui.h

--- ui.h
+++ ui.h
@@ -14,12 +14,16 @@ struct KeyAction {
     DWORD lastExecutionTime = 0; 
 };
 
+// --- Global Data ---
 extern std::map<int, std::vector<KeyAction>> g_complex_mappings;
 extern std::map<int, bool> g_physical_key_status; 
 extern std::map<int, float> g_key_states;
 extern int g_last_vk_code;
 extern std::string g_last_key_name;
 extern bool g_safety_mode_active; 
+
+// متغيرات جديدة للإدارة
+extern std::vector<std::pair<int, bool>> g_pending_inputs; // طابور انتظار للإرسال الآمن
 
 enum class AppState {
     Dashboard, Wizard_WaitForOriginal, Wizard_WaitForTarget, Wizard_AskSwap, Wizard_Configure        
@@ -28,8 +32,7 @@ extern AppState g_app_state;
 
 void InstallHooks();
 void UninstallHooks();
-void ProcessRapidFire();
-void UpdateAnimationState();
+void ProcessInputLogic(); // دالة المعالجة المجمعة
 void RenderUI();
 
 std::string GetKeyNameSmart(int vkCode);
